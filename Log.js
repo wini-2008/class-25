e9176c96c1e9a9b7fbe5ca0f6043b8d7b81127fa
wini-2008 <71551067+wini-2008@users.noreply.github.com>
2020-10-02 16:48:56 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,11 @@
+class Log extends BaseClass{
+    constructor(x, y, height, angle) {
+          
+      super(x,y,30,height,angle);
+      Matter.Body.setAngle(this.body, angle);
+
+      this.image = loadImage("sprites/wood2.png");
+    }
+  
+  };
+  
